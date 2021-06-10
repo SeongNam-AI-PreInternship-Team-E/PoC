9c1876942082b0608f6cda6deacef27c2b845b8d
ww8007 <wshmin@naver.com>
2021-06-10 18:36:55 +0900

    [feat] add_addTimes_reducer 개별 시간을 담는 addTimes reducer 추가

--- poc/src/modules/times/times.ts
+++ poc/src/modules/times/times.ts
@@ -1,17 +1,18 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { time } from "console";
 
 export type Times = {
   start: number;
   end: number;
 };
 
-export type Days = {
+export type SetDays = {
   id: number;
   day: number;
-  start_end: [];
+  start_end: Array<Times>;
 };
 
-export type DaysState = Days[];
+export type DaysState = SetDays[];
 
 let nextId = 1;
 const initialState: DaysState = [];
@@ -23,9 +24,24 @@ export const timeSlice = createSlice({
     addDays: (state, action: PayloadAction<number>) => {
       state.push({ id: nextId++, day: action.payload, start_end: [] });
     },
+    addTimes: (
+      state,
+      action: PayloadAction<{ id: number; set_time: Times }>
+    ) => {
+      const todo = state.find((todo) => todo.id === action.payload.id);
+      if (todo) {
+        todo.start_end.push(action.payload.set_time);
+      }
+    },
+    // toggleTodo: (state, action: { id: number }) => {
+    //   const todo = state.find((todo) => todo.id === action.payload);
+    //   if (todo) {
+    //     todo.check = !todo.check;
+    //   }
+    // },
   },
 });
 
-export const { addDays } = timeSlice.actions;
+export const { addDays, addTimes } = timeSlice.actions;
 
 export default timeSlice.reducer;
