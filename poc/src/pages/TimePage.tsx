183e3a338373b845de658d673ec10fec7a7960ee
ww8007 <wshmin@naver.com>
2021-06-10 18:37:31 +0900

    [feat] add_TimesContainer 시간 페이지에 컨테이너 추가

--- poc/src/pages/TimePage.tsx
+++ poc/src/pages/TimePage.tsx
@@ -1,6 +1,11 @@
 // @flow
 import * as React from "react";
+import TimesContainer from "../containers/TimesContainer";
 type Props = {};
 export const TimePage = (props: Props) => {
-  return <div></div>;
+  return (
+    <>
+      <TimesContainer></TimesContainer>
+    </>
+  );
 };
