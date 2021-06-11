c3f06ddbfff94eaba549f8023ebb101d1310ccb4
ww8007 <wshmin@naver.com>
2021-06-11 22:31:09 +0900

    [feat] add_TimesContainer 팀원들의 시간표를 보여주는 컨테이너 추가

--- poc/src/pages/TimePage.tsx
+++ poc/src/pages/TimePage.tsx
@@ -1,11 +1,13 @@
 // @flow
 import * as React from "react";
 import TimesContainer from "../containers/TimesContainer";
+import TeamTimesContainer from "../containers/TeamTimesContainer";
 type Props = {};
 export const TimePage = (props: Props) => {
   return (
     <>
       <TimesContainer></TimesContainer>
+      <TeamTimesContainer></TeamTimesContainer>
     </>
   );
 };
