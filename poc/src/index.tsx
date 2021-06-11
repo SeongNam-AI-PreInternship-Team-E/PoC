8ce6c5b3c67cff66b0a4b66e3faadf08e5f43e7b
ww8007 <wshmin@naver.com>
2021-06-12 00:23:26 +0900

    [feat] git page

--- poc/src/index.tsx
+++ poc/src/index.tsx
@@ -6,15 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./modules";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
 
 const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
