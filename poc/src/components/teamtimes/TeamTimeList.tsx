4ddcea0f16ab4c45e35170e84cfe3c411065ab48
ww8007 <wshmin@naver.com>
2021-06-11 22:37:52 +0900

    [feat] add_TeamTimeList_Component 팀원 시간표 리스트들 모아서 map으로 뿌려주는 컴포넌트 추가

---
+++ poc/src/components/teamtimes/TeamTimeList.tsx
@@ -0,0 +1,57 @@
+// @flow
+import React, { useEffect } from "react";
+import { SetDays } from "../../modules/times/times";
+import styled from "styled-components";
+import { Days } from "../../modules/days/days";
+import { TeamTimeItem } from "./TeamTimeItem";
+const TimesBlock = styled.div``;
+const DayBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-right: 2rem;
+`;
+
+type TimesProps = {
+  times: any;
+  onAddTeamTimes: (id: number, start: number, end: number) => void;
+  avalibaleTime: number;
+  timeSet: any;
+  startTime: number;
+  onAddTeamAllTimes: (id: number, arr: any) => void;
+  onChangeColor: (id: number, arrnum: number) => void;
+  teamtimes: any;
+};
+export const TeamTimeList = ({
+  times,
+  onAddTeamTimes,
+  timeSet,
+  startTime,
+  onAddTeamAllTimes,
+  onChangeColor,
+  teamtimes,
+}: TimesProps) => {
+  return (
+    <>
+      <TimesBlock>
+        <h1>팀원 시간</h1>
+
+        <DayBlock>
+          {times.map((day: any) => (
+            <TeamTimeItem
+              teametimes={teamtimes}
+              indexofTime={day.id}
+              onChangeColor={onChangeColor}
+              key={day.id}
+              time={day}
+              onAddTeamTimes={onAddTeamTimes}
+              onAddTeamAllTimes={onAddTeamAllTimes}
+              timeSet={timeSet}
+              startTime={startTime}
+            ></TeamTimeItem>
+          ))}
+        </DayBlock>
+      </TimesBlock>
+    </>
+  );
+};
