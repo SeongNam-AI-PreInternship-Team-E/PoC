a771e44bb833ad9abcb7561cd5c7f2f627fd89df
ww8007 <wshmin@naver.com>
2021-06-11 22:38:58 +0900

    [feat] add_changecolor 개인 시간표에 대한 컬러 업데이트 함수 추가

--- poc/src/components/times/TimesList.tsx
+++ poc/src/components/times/TimesList.tsx
@@ -1,5 +1,5 @@
 // @flow
-import * as React from "react";
+import React, { useEffect } from "react";
 import { SetDays } from "../../modules/times/times";
 import styled from "styled-components";
 import { Days } from "../../modules/days/days";
@@ -8,6 +8,8 @@ const TimesBlock = styled.div``;
 const DayBlock = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  padding-right: 2rem;
 `;
 
 type TimesProps = {
@@ -15,17 +17,33 @@ type TimesProps = {
   onAddTimes: (id: number, start: number, end: number) => void;
   avalibaleTime: number;
   timeSet: any;
+  startTime: number;
+  onAddAllTimes: (id: number, arr: any) => void;
+  onChangeColor: (id: number, arrnum: number) => void;
 };
-export const TimesList = ({ times, onAddTimes, timeSet }: TimesProps) => {
+export const TimesList = ({
+  times,
+  onAddTimes,
+  timeSet,
+  startTime,
+  onAddAllTimes,
+  onChangeColor,
+}: TimesProps) => {
   return (
     <>
       <TimesBlock>
+        <h1>나의 시간</h1>
         <DayBlock>
           {times.map((day) => (
             <TimeItem
+              indexofTime={day.id}
+              onChangeColor={onChangeColor}
+              key={day.id}
               time={day}
               onAddTimes={onAddTimes}
+              onAddAllTimes={onAddAllTimes}
               timeSet={timeSet}
+              startTime={startTime}
             ></TimeItem>
           ))}
         </DayBlock>
