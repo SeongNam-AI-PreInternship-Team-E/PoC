167917916289d74f5de6d3456fad801bf76b16cf
ww8007 <wshmin@naver.com>
2021-06-11 22:37:13 +0900

    [feat] add_TeamTimeItem 팀원 시간표 요일별 인덱스 추가

---
+++ poc/src/components/teamtimes/TeamTimeItem.tsx
@@ -0,0 +1,61 @@
+import { createPublicKey } from "crypto";
+import React, { useEffect } from "react";
+
+import styled from "styled-components";
+import { Days } from "../../modules/days/days";
+import { SetDays } from "../../modules/times/times";
+import { TeamTimeIndex } from "./TeamTimeIndex";
+const PageWrapper = styled.div`
+  width: 5rem;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const TimeBlock = styled.li`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  margin-left: 30px;
+`;
+
+type TimeProps = {
+  time: any[] | any;
+  onAddTeamTimes: (id: number, start: number, end: number) => void;
+  onAddTeamAllTimes: (id: number, arr: any) => void;
+  timeSet: any;
+  startTime: number;
+  onChangeColor: (id: number, arrnum: number) => void;
+  indexofTime: number;
+  teametimes: any;
+};
+
+export const TeamTimeItem = ({
+  time,
+  onAddTeamTimes,
+  timeSet,
+  onAddTeamAllTimes,
+  startTime,
+  onChangeColor,
+  indexofTime,
+  teametimes,
+}: TimeProps) => {
+  return (
+    <PageWrapper>
+      {console.log(time.all_times)}
+      <TimeBlock>
+        {time.all_times[0]
+          ? time.all_times[0].map((sep: any) => (
+              <TeamTimeIndex
+                time={time}
+                key={sep.index}
+                onChangeColor={onChangeColor}
+                indexofTime={indexofTime}
+                sep={sep}
+                teamtimes={teametimes}
+              />
+            ))
+          : null}
+      </TimeBlock>
+    </PageWrapper>
+  );
+};
